6aa2c5ec3f096d5ae7908b7f37ab78c08f897e9a
wangdongyang <dongyang.wang@broadlink.com.cn>
2020-07-09 15:07:33 +0800

    优化搜索

--- interview17-13re-space-lcci/re-space-lcci.go
+++ interview17-13re-space-lcci/re-space-lcci.go
@@ -2,10 +2,6 @@ package main
 
 import "math"
 
-func main() {
-
-}
-
 type Trie struct {
 	child  map[uint8]*Trie
 	MaxLen int
@@ -34,8 +30,29 @@ func (t *Trie) Add(word string) {
 	cur.isEnd = true
 
 }
+func (t *Trie) AddReverse(word string) {
 
-func (t *Trie) Exist(word []byte) bool {
+	cur := t
+	for i := len(word) - 1; i >= 0; i-- {
+		id := uint8(word[i] - uint8('a'))
+		child, ok := cur.child[id]
+		if ok {
+			cur = child
+		} else {
+			child = &Trie{child: make(map[uint8]*Trie, 0)}
+			cur.child[id] = child
+			cur = child
+		}
+	}
+
+	if len(word) > t.MaxLen {
+		t.MaxLen = len(word)
+	}
+
+	cur.isEnd = true
+
+}
+func (t *Trie) Find(word []byte) *Trie {
 
 	cur := t
 	for i := 0; i < len(word); i++ {
@@ -44,11 +61,28 @@ func (t *Trie) Exist(word []byte) bool {
 		if ok {
 			cur = child
 		} else {
-			return false
+			return nil
 		}
 	}
 
-	return cur.isEnd
+	return cur
+
+}
+
+func (t *Trie) FindReverse(word []byte) *Trie {
+
+	cur := t
+	for i := len(word) - 1; i >= 0; i-- {
+		id := uint8(word[i]) - uint8('a')
+		child, ok := cur.child[id]
+		if ok {
+			cur = child
+		} else {
+			return nil
+		}
+	}
+
+	return cur
 
 }
 
@@ -73,15 +107,22 @@ func (solution *Solution) RespaceHelp(beg int) int {
 
 	res = math.MaxInt32
 	for i := beg + 1; i <= min(beg+solution.Trie.MaxLen, solution.Count); i++ {
-		// for i := beg + 1; i <= min(beg+solution.Trie.MaxLen, solution.Count); i++ { //优化 从最长的开始匹配，如果在Trie中存在 就可以直接返回了
+
 		tmp := i - beg
-		if solution.Trie.Exist(solution.Sentence[beg:i]) {
+		node := solution.Trie.Find(solution.Sentence[beg:i])
+
+		if node != nil && node.isEnd {
 			tmp = 0
 		}
 
 		tmp = tmp + solution.RespaceHelp(i)
 		if tmp < res {
 			res = tmp
+		}
+
+		//如果 solution.Sentence[beg:i] 在trie中找不到节点，后面肯定也找不到节点
+		if node == nil {
+			break
 		}
 
 	}
@@ -91,7 +132,16 @@ func (solution *Solution) RespaceHelp(beg int) int {
 
 }
 
-func respace(dictionary []string, sentence string) int {
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+
+	return j
+}
+
+//534ms 28MB
+func respace1(dictionary []string, sentence string) int {
 
 	trie := &Trie{child: make(map[uint8]*Trie, 0)}
 	for i := range dictionary {
@@ -108,10 +158,53 @@ func respace(dictionary []string, sentence string) int {
 	return solution.RespaceHelp(0)
 }
 
-func min(i, j int) int {
-	if i < j {
-		return i
+//172ms 33MB
+func respace(dictionary []string, sentence string) int {
+
+	trie := &Trie{child: make(map[uint8]*Trie, 0)}
+	for i := range dictionary {
+		trie.AddReverse(dictionary[i])
 	}
 
-	return j
+	word := []byte(sentence)
+	//表示 前i中的最小
+	dp := make([]int, len(word)+1)
+	// minRes := math.MaxInt32
+
+	for i := 1; i <= len(word); i++ {
+
+		res := math.MaxInt32
+		cur := trie
+		// [0...j...i] -> [0,j] (j,i]
+		for j := i - 1; j >= 0; j-- {
+
+			node, ok := cur.child[uint8(word[j])-uint8('a')]
+			if ok == false {
+				if i-j+dp[j] < res {
+					res = i - j + dp[j]
+				}
+			} else if node.isEnd {
+				if dp[j] < res {
+					res = dp[j]
+				}
+			} else {
+				if i-j+dp[j] < res {
+					res = i - j + dp[j]
+				}
+			}
+
+			cur = node
+
+			if ok == false {
+				break
+			}
+
+		}
+
+		dp[i] = res
+
+	}
+
+	return dp[len(sentence)]
+
 }
