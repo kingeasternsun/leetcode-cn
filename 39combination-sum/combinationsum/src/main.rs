6f338504d7c5be5644fa02fce6f3276f9ee86f83
wangdongyang <dongyang.wang@broadlink.com.cn>
2020-09-09 10:43:29 +0800

    use take_while

--- 39combination-sum/combinationsum/src/main.rs
+++ 39combination-sum/combinationsum/src/main.rs
@@ -53,35 +53,34 @@ impl Solution {
         candidates.sort();
         let mut result = Vec::new();
         let mut tmp = Vec::new();
-        Self::dfs(& candidates,target,& mut result,& mut tmp,None,0);
+        Self::dfs(& candidates,target,& mut result,& mut tmp,0);
 
         result
 
 
     }
 
-    fn dfs(candidates:  & Vec<i32>, target: i32, result :& mut Vec<Vec<i32>>, tmp : & mut Vec<i32>,tmp_id : Option<usize>,tmp_sum :i32){
+    /*
+     target 表示距离目标还差多少 
+     tmp 表示当前已经装入了哪些
+     tmp_id 表示 下一层级 从第几个开始
+    */
 
-        if tmp_sum == target{
+    fn dfs(candidates:  & Vec<i32>, target: i32, result :& mut Vec<Vec<i32>>, tmp : & mut Vec<i32>,tmp_id : usize){
+
+        if 0 == target{
             result.push(tmp.clone());
             return 
         }
-        if tmp_sum > target{
-            // result.push(tmp.clone());
+        if candidates[tmp_id] > target{
             return 
         }
 
 
-        let mut beg = 0;
-        if let Some(end) = tmp_id{
-            beg = end;
-        }
+        for i in (tmp_id..candidates.len()).take_while(|&x| candidates[x]<=target){
 
-        for i in beg..candidates.len(){
-
-            let new_sum = tmp_sum + candidates[i];
             tmp.push(candidates[i]);
-            Self::dfs(candidates,target,result,tmp,Some(i),new_sum);
+            Self::dfs(candidates,target-candidates[i],result,tmp,i);
             tmp.pop();
 
         }
