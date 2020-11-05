51c800a7f7ea2ee49e70017772d0ee5f7c6acea6
wangdongyang <dongyang.wang@broadlink.com.cn>
2020-11-05 15:40:10 +0800

    双边BFS

--- 127ladderLength/ladder_length/src/main.rs
+++ 127ladderLength/ladder_length/src/main.rs
@@ -13,6 +13,8 @@ fn main() {
         Solution::ladder_length(String::from("hit"), String::from("cog"), word_list),
         5
     );
+
+    
     let word_list2 = vec![
         String::from("hot"),
         String::from("dot"),
@@ -36,6 +38,18 @@ fn main() {
         Solution::ladder_length(String::from("hot"), String::from("dog"), word_list3),
         3
     );
+
+    let word_list4 = vec![
+        String::from("a"),
+        String::from("b"),
+        String::from("c"),
+    ];
+    assert_eq!(
+        Solution::ladder_length(String::from("a"), String::from("c"), word_list4),
+        2
+    );
+
+    
 }
 pub struct Solution;
 
@@ -43,9 +57,9 @@ pub struct Solution;
 impl Solution {
     /*
     1. BFS
-    2. 从 begin_word end_word分别轮流开始遍历 。
+    2. 从 begin_word 开始 220ms  2.7MB
     */
-    pub fn ladder_length(begin_word: String, end_word: String, word_list: Vec<String>) -> i32 {
+    pub fn ladder_length_dfs(begin_word: String, end_word: String, word_list: Vec<String>) -> i32 {
         let mut exist = false;
         for v in &word_list {
             if *v == end_word {
@@ -64,6 +78,7 @@ impl Solution {
         let mut queue = std::collections::VecDeque::new();
         queue.push_back(begin_word.clone());
         word.insert(begin_word.clone());
+
 
         let mut step = 1;
 
@@ -111,5 +126,80 @@ impl Solution {
         }
 
         return cnt == 1;
+    }
+
+    //双边BFS 20ms 2.5MB
+    pub fn ladder_length(begin_word: String, end_word: String, word_list: Vec<String>) -> i32 {
+
+        let mut exist = false;
+        let mut end_id = word_list.len() ;
+        for i in 0..word_list.len() {
+            if word_list[i] == end_word {
+                exist = true;
+                end_id = i;
+                break;
+            }
+        }
+
+        if !exist {
+            return 0;
+        }
+
+
+
+        let mut flags = vec![0;word_list.len()];
+
+        //两个队列分别用于两边BFS
+        let mut first_queue = std::collections::VecDeque::new();
+        first_queue.push_back(&begin_word);
+
+        let mut second_queue = std::collections::VecDeque::new();
+        second_queue.push_back(&end_word);
+        flags[end_id] = 2; //这里要注意，end_word已经加入了右边的队列，所以要赋值2
+
+        let mut cur_queue;
+        let mut flag;
+        let mut step = 1;
+        while first_queue.len()>0 && second_queue.len()>0 {
+
+            if first_queue.len()>second_queue.len(){
+                cur_queue = & mut second_queue;
+                flag = 2;
+            }else{
+                //如果大小相同，就从左边开始 
+                cur_queue = & mut first_queue;
+                flag = 1;
+            }
+
+            let num = cur_queue.len();
+
+            for _ in 0..num{
+
+                if let Some(node) = cur_queue.pop_front(){
+
+
+                    for i in 0..word_list.len() {
+                        //如果词典里的这个单词和当前节点的单词 差一个字符,并且这个方向没有访问过
+                        if Self::one_char_diff(&node, &word_list[i]) && (flags[i] != flag) {
+
+                            flags[i]+=flag;
+                            if flags[i] ==3{
+                                return step+1;
+                            }
+                            cur_queue.push_back(&word_list[i]);
+
+                        }
+                    }
+
+
+                }
+            }
+            step+=1;
+
+        }
+
+
+
+        0
     }
 }
