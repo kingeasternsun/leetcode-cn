e20eed4511c9e5ecc5c736c8440def2b5944e0aa
wangdongyang <dongyang.wang@broadlink.com.cn>
2021-01-05 09:41:31 +0800

    145后续遍历

--- 145binary-tree-postorder-traversal/145binary-tree-postorder-traversal.go
+++ 145binary-tree-postorder-traversal/145binary-tree-postorder-traversal.go
@@ -110,3 +110,41 @@ func postorderTraversal(root *TreeNode) []int {
 	}
 
 }
+
+func postorderTraversal3(root *TreeNode) []int {
+
+	cur := root
+	stack := make([]*TreeNode, 0)
+	res := make([]int, 0)
+
+	// 用于记录上次的访问（加入到res）的节点，因为是后续遍历，所以如果当前节点cur有Right节点，并且Right节点刚访问过（加入到了res中），那么
+	// Right节点所对应的子树肯定都已经遍历过了，而且这个Right节点是最后一个访问的。 既然当前节点cur的Right已经访问过了，那么cur就可以访问了
+	var pre *TreeNode
+	for {
+		//一路向左入栈
+		for cur != nil {
+			stack = append(stack, cur)
+			cur = cur.Left
+		}
+
+		if len(stack) == 0 {
+			return res
+		}
+
+		//获取栈顶的点，注意这里先不要出栈
+		top := stack[len(stack)-1]
+		if top.Right != nil && top.Right != pre { //如果top节点的Right节点还没有访问，那么就要先访问top的右边节点，top还保留栈中
+			cur = top.Right
+
+		} else { //top节点的Right节点不存在 或已经访问过了，那么就可以访问当前节点了
+
+			res = append(res, top.Val)
+			stack = stack[:len(stack)-1] //这里就可以出栈了
+
+			pre = top //记录pre
+		}
+
+	}
+
+	return
+}
