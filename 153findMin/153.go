37eb0421d280fc51ffa8f41cfa0d05ff314c20f8
kingeasternsun <kingeasternsun@gmail.com>
2021-04-08 22:33:00 +0800

    153

--- 153findMin/153.go
+++ 153findMin/153.go
@@ -1,3 +1,13 @@
+/*
+ * @Description:
+ * @Version: 2.0
+ * @Author: kingeasternsun
+ * @Date: 2021-01-24 16:17:56
+ * @LastEditors: kingeasternsun
+ * @LastEditTime: 2021-04-08 22:32:53
+ * @FilePath: /153findMin/153.go
+ */
+
 package main
 
 //https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
@@ -6,7 +16,7 @@ package main
  - 如果mid的数值大于right的数值，说明mid在高数值的数据范围内，更小的在右边。所以在 (mid,right] 里面找
 */
 //153. 寻找旋转排序数组中的最小值 0ms 2.2MB
-func findMin(nums []int) int {
+func findMin1(nums []int) int {
 	inLen := len(nums)
 	if inLen == 0 {
 		return -1
@@ -20,9 +30,6 @@ func findMin(nums []int) int {
 	right := inLen - 1
 
 	var mid int
-	// if left<=right{
-	// 	return nums[left]
-	// }
 
 	for left <= right {
 		if left == right {
@@ -39,5 +46,39 @@ func findMin(nums []int) int {
 	}
 
 	return 0
+
+}
+
+func findMin(nums []int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	//如果最后一个比第一个要大，说明是递增的
+	if nums[len(nums)-1] > nums[0] {
+		return nums[0]
+	}
+
+	beg := 0
+	end := len(nums) - 1
+	for beg < end {
+
+		if nums[beg] < nums[end] {
+			return nums[beg]
+		}
+
+		mid := (end-beg)/2 + beg
+		// 在左边的递增数组上,这里最关键，要从mid  和 end上的值进行比较，因为 mid 有可能就是 beg
+		// 特别容易错的地方就在于 ben = end-1 的情况时候
+		if nums[mid] > nums[end] {
+			beg = mid + 1
+			continue
+		}
+
+		end = mid
+
+	}
+
+	return nums[beg]
 
 }
