87906242f4cc4b98dd8f034e314920db6e81afc2
kingeasternsun <kingeasternsun@gmail.com>
2021-11-14 19:42:13 +0800

    单循环优化

--- 520detectCapitalUse/detect_capital_use/src/main.rs
+++ 520detectCapitalUse/detect_capital_use/src/main.rs
@@ -3,27 +3,43 @@ fn main() {
 }
 
 pub fn detect_capital_use(word: String) -> bool {
-    if word.len() == 0 {
-        return true;
-    }
-    if word.len() == 1 {
-        return true;
-    }
 
-    if word.chars().all(|x| x.is_lowercase())//都是小写
-        || word.chars().all(|x| x.is_uppercase())//都是大写
-        || word.chars().enumerate().all(|(id, x)| {// 首字符大写，剩余小写
-            if id == 0 {
-                x.is_uppercase()
-            } else {
-                x.is_lowercase()
+    let mut word = word.chars();
+    let first = word.next();
+    if first.is_none(){
+        return true
+    }
+    let first = first.unwrap();
+
+    if let Some(second) = word.next(){
+        let res = word.try_for_each(move |x|{
+            if second.is_lowercase() && x.is_lowercase(){
+                return Ok(())
             }
-        })
-    {
-        return true;
+    
+            if second.is_uppercase() && x.is_uppercase(){
+                return Ok(())
+            }
+    
+            Err(())
+        });
+
+        if res.is_err(){
+            return false
+        }
+        if first.is_uppercase(){
+            return true
+        }
+
+        if first.is_lowercase() && second.is_lowercase(){
+            return true
+        }
+    
+        false
+    }else{
+        true
     }
 
-    false
 }
 #[cfg(test)]
 mod tests {
@@ -31,9 +47,14 @@ mod tests {
 
     #[test]
     fn test_name() {
+        assert!(detect_capital_use("U".to_string()));
+        assert!(detect_capital_use("US".to_string()));
         assert!(detect_capital_use("USA".to_string()));
+        assert!(detect_capital_use("a".to_string()));
+        assert!(detect_capital_use("ab".to_string()));
         assert!(detect_capital_use("abc".to_string()));
         assert!(detect_capital_use("Abc".to_string()));
+        assert!(detect_capital_use("Ab".to_string()));
         assert!(!detect_capital_use("bAc".to_string()));
         assert!(!detect_capital_use("FlaG".to_string()));
     }
