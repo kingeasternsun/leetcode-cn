29b9caddc9fc48a47329c03f1b5b966597ac0ecb
kingeasternsun <kingeasternsun@gmail.com>
2021-11-14 18:48:14 +0800

    合并判断

--- 520detectCapitalUse/detect_capital_use/src/main.rs
+++ 520detectCapitalUse/detect_capital_use/src/main.rs
@@ -3,29 +3,27 @@ fn main() {
 }
 
 pub fn detect_capital_use(word: String) -> bool {
-
-    if word.len() ==0{
-        return true
+    if word.len() == 0 {
+        return true;
     }
-    if word.len() ==1{
-        return true
-    }
-
-    if word.chars().all(|x|x.is_lowercase()){
-        return true 
+    if word.len() == 1 {
+        return true;
     }
 
-    if word.chars().all(|x|x.is_uppercase()){
-        return true 
+    if word.chars().all(|x| x.is_lowercase())//都是小写
+        || word.chars().all(|x| x.is_uppercase())//都是大写
+        || word.chars().enumerate().all(|(id, x)| {// 首字符大写，剩余小写
+            if id == 0 {
+                x.is_uppercase()
+            } else {
+                x.is_lowercase()
+            }
+        })
+    {
+        return true;
     }
-
-    if word.chars().enumerate().all(|(id,x)|if id == 0{x.is_uppercase()} else{x.is_lowercase()}){
-        return true 
-    }
-    
 
     false
-
 }
 #[cfg(test)]
 mod tests {
