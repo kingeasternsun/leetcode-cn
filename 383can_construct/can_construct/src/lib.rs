15eca28fb944f3bfe2d3c534f7b3b89442a79fb6
kingeasternsun <kingeasternsun@gmail.com>
2021-12-04 20:04:27 +0800

    try_for_each to all

--- 383can_construct/can_construct/src/lib.rs
+++ 383can_construct/can_construct/src/lib.rs
@@ -42,6 +42,59 @@ mod leet383 {
         }
     }
 }
+mod leet383_b {
+    struct Solution;
+    impl Solution {
+        pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+            let mut m = vec![0u32; 26];
+            magazine
+                .as_bytes()
+                .iter()
+                .for_each(|x| m[(x - b'a') as usize] = m[(x - b'a') as usize] + 1);
+
+            ransom_note.as_bytes().iter().all(|x| {
+                if m[(x - b'a') as usize] == 0 {
+                    return false;
+                }
+                m[(x - b'a') as usize] = m[(x - b'a') as usize] - 1;
+                true
+            })
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::Solution;
+
+        #[test]
+        fn it_works() {
+            assert_eq!(
+                Solution::can_construct("a".to_string(), "b".to_string()),
+                false
+            );
+            assert_eq!(
+                Solution::can_construct("aa".to_string(), "ab".to_string()),
+                false
+            );
+            assert_eq!(
+                Solution::can_construct("aa".to_string(), "aab".to_string()),
+                true
+            );
+            assert_eq!(
+                Solution::can_construct("aab".to_string(), "aab".to_string()),
+                true
+            );
+            assert_eq!(
+                Solution::can_construct("aab".to_string(), "aa".to_string()),
+                false
+            );
+            assert_eq!(
+                Solution::can_construct("aab".to_string(), "ab".to_string()),
+                false
+            );
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     use crate::Solution;
