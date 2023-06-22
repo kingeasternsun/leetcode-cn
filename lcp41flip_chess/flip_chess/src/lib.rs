c7b61d6c54a8cd9a17789c8fd0051999b64d80a0
kingeasternsun <kingeasternsun@gmail.com>
2023-06-22 16:28:45 +0000

    lcp41, interview 16.19

--- lcp41flip_chess/flip_chess/src/lib.rs
+++ lcp41flip_chess/flip_chess/src/lib.rs
@@ -1,8 +1,7 @@
 pub struct Solution;
-
 pub struct Queue {
-    data: Vec<(i32, i32)>,//record the 'O' that has been fliped
-    front: usize, // record the index of front element
+    data: Vec<(i32, i32)>, //record the 'O' that has been fliped
+    front: usize,          // record the index of front element
 }
 impl Queue {
     pub fn new() -> Self {
@@ -28,15 +27,17 @@ impl Queue {
         self.data.len()
     }
 }
+
+use std::option::Option;
 impl Solution {
-    fn can_flip(chessboard: &Vec<Vec<char>>, cur: (i32, i32), dir: &(i32, i32)) -> (i32, bool) {
+    fn can_flip(chessboard: &Vec<Vec<char>>, cur: (i32, i32), dir: &(i32, i32)) -> Option<i32> {
         let board_size = (chessboard.len() as i32, chessboard[0].len() as i32);
         let mut cur = (cur.0 + dir.0, cur.1 + dir.1);
         let mut n = 0; // the number of 'O'
         while cur.0 >= 0 && cur.0 < board_size.0 && cur.1 >= 0 && cur.1 < board_size.1 {
             match chessboard[cur.0 as usize][cur.1 as usize] {
-                '.' => return (n, false),
-                'X' => return (n, true),
+                '.' => return Option::None,
+                'X' => return Option::Some(n),
                 _ => {
                     cur.0 += dir.0;
                     cur.1 += dir.1;
@@ -45,7 +46,7 @@ impl Solution {
             }
         }
 
-        (n, false)
+        Option::None
     }
 
     pub fn flip_chess(chessboard: Vec<String>) -> i32 {
@@ -74,34 +75,29 @@ impl Solution {
                 let mut queue = Queue::new();
                 chessboard[i][j] = 'X';
                 queue.push((i as i32, j as i32));
-                while queue.len() >0 {
+                while queue.len() > 0 {
                     let cur = queue.pop();
-                    for dir in &dirs{
-                        let ret = Self::can_flip(&chessboard, (cur.0 as i32, cur.1 as i32), dir);
-                        if !ret.1{
-                            continue;
+                    for dir in &dirs {
+                        if let Some(n) = Self::can_flip(&chessboard, (cur.0 as i32, cur.1 as i32), dir){
+                            let mut tmp = cur;
+                            for _ in 0..n {
+                                tmp = (tmp.0 + dir.0, tmp.1 + dir.1);
+                                chessboard[tmp.0 as usize][tmp.1 as usize] = 'X';
+                                queue.push(tmp);
+                            }
                         }
-                        
-                        let mut tmp = cur;
-                        for _ in 0..ret.0{
-                            tmp = (tmp.0 + dir.0, tmp.1 + dir.1);
-                            chessboard[tmp.0 as usize][tmp.1 as usize] = 'X';
-                            queue.push(tmp);
-                        }
-
                     }
                 }
 
-                if queue.data_size()-1 > ans{
-                    ans = queue.data_size()-1;
+                if queue.data_size() - 1 > ans {
+                    ans = queue.data_size() - 1;
                 }
 
-                for p in queue.data{
+                for p in queue.data {
                     chessboard[p.0 as usize][p.1 as usize] = 'O';
                 }
 
                 chessboard[i][j] = '.';
-
             }
         }
         ans as i32
@@ -114,25 +110,27 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(Solution::flip_chess(vec![
-            String::from("....X."),
-            String::from("....X."),
-            String::from("XOOO.."),
-            String::from("......"),
-            String::from("......"),
-        ]), 3);
-
-    
+        assert_eq!(
+            Solution::flip_chess(vec![
+                String::from("....X."),
+                String::from("....X."),
+                String::from("XOOO.."),
+                String::from("......"),
+                String::from("......"),
+            ]),
+            3
+        );
     }
 
     #[test]
     fn it_works2() {
-
-        assert_eq!(Solution::flip_chess(vec![
-            String::from(".X."),
-            String::from(".O."),
-            String::from("XO."),
-        ]), 2);
-    
+        assert_eq!(
+            Solution::flip_chess(vec![
+                String::from(".X."),
+                String::from(".O."),
+                String::from("XO."),
+            ]),
+            2
+        );
     }
 }
