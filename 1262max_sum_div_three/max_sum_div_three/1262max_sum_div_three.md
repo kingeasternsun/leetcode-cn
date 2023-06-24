681295f58f33e2e1833a0398f39ad2be6258e780
kingeasternsun <kingeasternsun@gmail.com>
2023-06-24 11:55:10 +0800

    :art: 1262 use chunks_exact

---
+++ 1262max_sum_div_three/max_sum_div_three/1262max_sum_div_three.md
@@ -0,0 +1,44 @@
+
+# 第一种解法
+
+1. 如果总数模3剩余1，那么就减去模1的最小数或两个最小的模2的数
+1. 如果总数模3剩余2，那么就减去模2的最小数或两个最小的模1的数
+
+在Rust实现中，我们取Vec中前面的两个数字并求和，最普通的做法是
+```Rust
+let r2: Option<i32> = match mod2.len() {
+    2 => Some(mod2[0] + mod2[1]),
+    _ => None,
+};
+```
+
+高级一些的做法是:
+```Rust
+
+let r2 = mod2.chunks_exact(2).next().map(|x| x[0] + x[1]);
+```
+这里要注意 `chunks`, `chunks_exact` 两个方法的不同，`chunks_exact` 会保证生成的每一个chunk的长度都是满足chunk_size大小的，这个题目中我们要用 `chunks_exact`
+```Rust
+
+    let slice = ['l', 'o', 'r', 'e', 'm'];
+    let mut iter = slice.chunks_exact(2);
+    assert_eq!(iter.next().unwrap(), &['l', 'o']);
+    assert_eq!(iter.next().unwrap(), &['r', 'e']);
+    assert!(iter.next().is_none());
+    assert_eq!(iter.remainder(), &['m']);
+```
+
+```Rust
+    let slice = ['l', 'o', 'r', 'e', 'm'];
+    let mut iter = slice.chunks(2);
+    assert_eq!(iter.next().unwrap(), &['l', 'o']);
+    assert_eq!(iter.next().unwrap(), &['r', 'e']);
+    assert_eq!(iter.next().unwrap(), &['m']);
+    assert!(iter.next().is_none());
+```
+
+更高级的用法
+```Rust
+
+
+```
--- 1262max_sum_div_three/max_sum_div_three/src/lib.rs
+++ 1262max_sum_div_three/max_sum_div_three/src/lib.rs
@@ -1,4 +1,3 @@
-
 struct Solution;
 impl Solution {
     pub fn max_sum_div_three1(nums: Vec<i32>) -> i32 {
@@ -28,10 +27,7 @@ impl Solution {
 
         if sum % 3 == 1 {
             let r1 = mod1.first();
-            let r2: Option<i32> = match mod2.len() {
-                2 => Some(mod2[0] + mod2[1]),
-                _ => None,
-            };
+            let r2 = mod2.chunks_exact(2).next().map(|x| x[0] + x[1]);
 
             return match (r1, r2) {
                 (Some(n1), Some(n2)) => (sum - n1).max(sum - n2),
@@ -42,10 +38,7 @@ impl Solution {
         }
 
         if sum % 3 == 2 {
-            let r1: Option<i32> = match mod1.len() {
-                2 => Some(mod1[0] + mod1[1]),
-                _ => None,
-            };
+            let r1: Option<i32> = mod1.chunks_exact(2).next().map(|x| x[0] + x[1]);
             let r2 = mod2.first();
 
             return match (r1, r2) {
@@ -62,7 +55,7 @@ impl Solution {
     pub fn max_sum_div_three(nums: Vec<i32>) -> i32 {
         // note: the init i32::MIN is import
         let ret = nums.iter().fold([0, i32::MIN, i32::MIN], |sum, &item| {
-            let mut tmp = [0,0,0];
+            let mut tmp = [0, 0, 0];
             for i in 0..3 {
                 let newid = ((i + item) % 3) as usize;
                 tmp[newid] = sum[newid].max(sum[i as usize] + item);
@@ -75,14 +68,14 @@ impl Solution {
 
 #[cfg(test)]
 mod tests {
-    // #[test]
-    // fn it_works() {
-    //     use crate::Solution;
-    //     assert_eq!(Solution::max_sum_div_three1(vec![3, 6, 5, 1, 8]), 18);
-    //     assert_eq!(Solution::max_sum_div_three1(vec![3, 6, 5, 1, 9]), 24);
-    //     assert_eq!(Solution::max_sum_div_three1(vec![4]), 0);
-    //     assert_eq!(Solution::max_sum_div_three1(vec![3]), 3);
-    // }
+    #[test]
+    fn it_works() {
+        use crate::Solution;
+        assert_eq!(Solution::max_sum_div_three1(vec![3, 6, 5, 1, 8]), 18);
+        assert_eq!(Solution::max_sum_div_three1(vec![3, 6, 5, 1, 9]), 24);
+        assert_eq!(Solution::max_sum_div_three1(vec![4]), 0);
+        assert_eq!(Solution::max_sum_div_three1(vec![3]), 3);
+    }
     #[test]
     fn dp_works() {
         use crate::Solution;
