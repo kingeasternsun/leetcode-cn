7ad003534094f117b86761291ca59e3934a2a884
kingeasternsun <kingeasternsun@gmail.com>
2023-07-20 06:59:06 +0000

    918 max_subarray_sum_circular

--- 918_max_subarray_sum_circular/max_subarray_sum_circular/src/lib.rs
+++ 918_max_subarray_sum_circular/max_subarray_sum_circular/src/lib.rs
@@ -16,7 +16,7 @@ impl Solution {
             res = res.max(pre_max_sum);
 
             tmp_left_sum += nums[i];
-            max_left_sum[i] = tmp_left_sum.max(max_left_sum[i-1]);
+            max_left_sum[i] = tmp_left_sum.max(max_left_sum[i - 1]);
         }
 
         let mut right_sum = 0;
@@ -28,27 +28,35 @@ impl Solution {
         res
     }
 
+    //
     pub fn max_subarray_sum_circular2(nums: Vec<i32>) -> i32 {
         if nums.len() == 0 {
             return 0;
         }
 
-        if nums.iter().all(|&x| x >= 0) {
-            return nums.iter().sum();
+        let sum:i32 = nums.iter().sum();
+
+        let mut max_sub_sum = nums[0];
+        let mut pre_max_sum = nums[0]; // the max subarray sum that end with id
+
+        let mut min_sub_sum = nums[0];
+        let mut pre_min_sum = nums[0]; // the min subarray sum that end with id
+
+        for i in 1..nums.len() {
+            pre_max_sum = nums[i].max(pre_max_sum + nums[i]);
+            max_sub_sum = max_sub_sum.max(pre_max_sum);
+
+            pre_min_sum = nums[i].min(pre_min_sum + nums[i]);
+            min_sub_sum = min_sub_sum.min(pre_min_sum);
         }
 
-        // we need recored the starting index and the max sum that end with current index
-        let mut dp = Vec::with_capacity(2 * nums.len());
-        for id in 0..2 * nums.len() {
-            let r_id = id % nums.len();
-            if id == 0 || dp[id - 1] <= 0 {
-                dp.push(nums[r_id]);
-            } else {
-                dp.push(dp[id - 1] + nums[r_id]);
-            }
-            // println!("{}", dp);
+        // which means all the num is negative values,
+        if max_sub_sum < 0{
+            max_sub_sum
+        }else{
+            max_sub_sum.max(sum - min_sub_sum)
         }
-        dp.into_iter().max().unwrap()
+       
     }
 }
 
@@ -58,15 +66,15 @@ mod tests {
 
     #[test]
     fn it_works() {
-        // assert_eq!(Solution::max_subarray_sum_circular2(vec![-1]), -1);
-        // assert_eq!(Solution::max_subarray_sum_circular2(vec![-1, -2]), -1);
+        assert_eq!(Solution::max_subarray_sum_circular2(vec![-1]), -1);
+        assert_eq!(Solution::max_subarray_sum_circular2(vec![-1, -2]), -1);
 
-        // assert_eq!(Solution::max_subarray_sum_circular2(vec![1]), 1);
-        // assert_eq!(Solution::max_subarray_sum_circular2(vec![1, 2]), 3);
-        // assert_eq!(Solution::max_subarray_sum_circular2(vec![1, 2, 3]), 6);
+        assert_eq!(Solution::max_subarray_sum_circular2(vec![1]), 1);
+        assert_eq!(Solution::max_subarray_sum_circular2(vec![1, 2]), 3);
+        assert_eq!(Solution::max_subarray_sum_circular2(vec![1, 2, 3]), 6);
 
-        // assert_eq!(Solution::max_subarray_sum_circular2(vec![5, -3, 5]), 10);
-        // assert_eq!(Solution::max_subarray_sum_circular2(vec![3, -2, 2, -3]), 3);
+        assert_eq!(Solution::max_subarray_sum_circular2(vec![5, -3, 5]), 10);
+        assert_eq!(Solution::max_subarray_sum_circular2(vec![3, -2, 2, -3]), 3);
     }
 
     #[test]
