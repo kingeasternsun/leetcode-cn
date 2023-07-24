a678e7fa3b14997800bfb9e88476f4c5a917a0e4
kingeasternsun <kingeasternsun@gmail.com>
2023-07-24 07:53:03 +0000

    874_robot_sim use iterator

--- 874_robot_sim/robot_sim/src/lib.rs
+++ 874_robot_sim/robot_sim/src/lib.rs
@@ -36,6 +36,40 @@ impl Solution {
 
         ret
     }
+    pub fn robot_sim2(commands: Vec<i32>, obstacles: Vec<Vec<i32>>) -> i32 {
+        // the beauty of iterator
+        let obs_set = obstacles
+            .iter()
+            .map(|x| (x[0], x[1]))
+            .collect::<std::collections::HashSet<_>>();
+        let mut ret = 0;
+        let dirs = vec![(0, 1), (1, 0), (0, -1), (-1, 0)];
+        let mut cur_dir_id = 0;
+        let mut cur_pos = (0, 0);
+        for cmd in commands {
+            if cmd == -1 {
+                // turn left
+                cur_dir_id = (cur_dir_id + 1) % 4;
+            } else if cmd == -2 {
+                cur_dir_id = (cur_dir_id + 3) % 4;
+            } else {
+                for _ in 0..cmd {
+                    let next_pos = (
+                        cur_pos.0 + dirs[cur_dir_id].0,
+                        cur_pos.1 + dirs[cur_dir_id].1,
+                    );
+                    if next_pos.0 < 0 || next_pos.1 < 0 || obs_set.get(&next_pos).is_some() {
+                        break;
+                    }
+
+                    cur_pos = next_pos;
+                    ret = ret.max(cur_pos.0 * cur_pos.0 + cur_pos.1 * cur_pos.1);
+                }
+            }
+        }
+
+        ret
+    }
 }
 
 #[cfg(test)]
@@ -47,6 +81,15 @@ mod tests {
         assert_eq!(Solution::robot_sim(vec![4, -1, 3], vec![]), 25);
         assert_eq!(
             Solution::robot_sim(vec![4, -1, 4, -2, 4], vec![vec![2, 4]]),
+            65
+        );
+    }
+
+    #[test]
+    fn it_works2() {
+        assert_eq!(Solution::robot_sim2(vec![4, -1, 3], vec![]), 25);
+        assert_eq!(
+            Solution::robot_sim2(vec![4, -1, 4, -2, 4], vec![vec![2, 4]]),
             65
         );
     }
