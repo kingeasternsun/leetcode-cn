4467205ea36c5fdb804e204704217981386e4dd1
kingeasternsun <kingeasternsun@gmail.com>
2023-07-26 02:21:52 +0000

    2208 halve_array rust with peek_mut

--- 2208_halve_array/halve_array/src/lib.rs
+++ 2208_halve_array/halve_array/src/lib.rs
@@ -35,7 +35,7 @@ impl Solution {
 
         ret
     }
-    pub fn halve_array4(nums: Vec<i32>) -> i32 {
+    pub fn halve_array2(nums: Vec<i32>) -> i32 {
         let mut ret = 0;
         let sum: f64 = nums.iter().map(|&x| x as f64).sum();
         let half_sum = sum / 2.0;
@@ -46,41 +46,61 @@ impl Solution {
             .map(|&x| Float64(x as f64))
             .collect::<std::collections::BinaryHeap<_>>();
 
-        while remove_sum < half_sum{
-            let x =  queue.pop().unwrap().0/2.0;
+        while remove_sum < half_sum {
+            let x = queue.pop().unwrap().0 / 2.0;
             remove_sum += x;
             ret += 1;
             queue.push(Float64(x));
-
         }
 
         ret
     }
-    
 
-    pub fn halve_array2(nums: Vec<i32>) -> i32 {
+    // Optimization: modified the peek in place instead of pop, modify and push
+    pub fn halve_array3(nums: Vec<i32>) -> i32 {
         let mut ret = 0;
+        // NOTE: the sum of the nums maybe overflow, so we need to convert it to f64 when sum
         let sum: f64 = nums.iter().map(|&x| x as f64).sum();
         let half_sum = sum / 2.0;
-        let mut left_sum = sum as f64;
+        let mut remove_sum = 0_f64;
 
         let mut queue = nums
             .iter()
             .map(|&x| Float64(x as f64))
             .collect::<std::collections::BinaryHeap<_>>();
-        while let Some(Float64(m)) = queue.pop() {
+        while let Some(mut m) = queue.peek_mut() {
             ret += 1;
-            left_sum = left_sum - m / 2.0;
-            if left_sum <= half_sum {
+            remove_sum = remove_sum + m.0 / 2.0;
+            if remove_sum < half_sum {
+                *m = Float64(m.0 / 2.0);
+            } else {
                 break;
             }
-            queue.push(Float64(m / 2.0));
         }
 
         ret
     }
 
+    pub fn halve_array4(nums: Vec<i32>) -> i32 {
+        let mut ret = 0;
+        let sum: f64 = nums.iter().map(|&x| x as f64).sum();
+        let half_sum = sum / 2.0;
+        let mut left_sum = sum;
 
+        let mut queue = nums
+            .iter()
+            .map(|&x| Float64(x as f64))
+            .collect::<std::collections::BinaryHeap<_>>();
+
+        while left_sum > half_sum {
+            let mut m = queue.peek_mut().unwrap();
+            left_sum -= m.0 / 2.0;
+            ret += 1;
+            *m = Float64(m.0 / 2.0);
+        }
+
+        ret
+    }
 }
 
 #[cfg(test)]
@@ -101,5 +121,21 @@ mod tests {
         assert_eq!(Solution::halve_array2(vec![3, 8, 20]), 3);
         assert_eq!(Solution::halve_array2(vec![2, 16, 20]), 3);
         assert_eq!(Solution::halve_array2(vec![2, 20]), 2);
+    }
+
+    #[test]
+    fn it_works3() {
+        assert_eq!(Solution::halve_array3(vec![5, 19, 8, 1]), 3);
+        assert_eq!(Solution::halve_array3(vec![3, 8, 20]), 3);
+        assert_eq!(Solution::halve_array3(vec![2, 16, 20]), 3);
+        assert_eq!(Solution::halve_array3(vec![2, 20]), 2);
+    }
+
+    #[test]
+    fn it_works4() {
+        assert_eq!(Solution::halve_array4(vec![5, 19, 8, 1]), 3);
+        assert_eq!(Solution::halve_array4(vec![3, 8, 20]), 3);
+        assert_eq!(Solution::halve_array4(vec![2, 16, 20]), 3);
+        assert_eq!(Solution::halve_array4(vec![2, 20]), 2);
     }
 }
