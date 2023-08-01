4049918d7fba0569153b29929e1f6a3a4da4abb1
kingeasternsun <kingeasternsun@gmail.com>
2023-08-01 22:42:41 +0800

    Rust 3 length_of_longest_substring

--- 3_length_of_longest_substring/length_of_longest_substring/src/lib.rs
+++ 3_length_of_longest_substring/length_of_longest_substring/src/lib.rs
@@ -3,20 +3,35 @@ struct Solution;
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
         let mut set = std::collections::HashSet::new();
+        let mut left = 0;
+        let mut right = 0;
         let mut ret = 0;
-        s.as_bytes().into_iter().for_each(|b| {
-            if set.contains(b) {
-                ret =ret.max(set.len());
-                set.clear();
-                set.insert(b);
-            } else {
-                set.insert(b);
+        let bytes = s.as_bytes();
+        // s.as_bytes().into_iter().for_each(|b| {
+        //     if set.contains(b) {
+        //         ret = ret.max(set.len());
+        //     }
+        //     set.insert(b);
+        // });
+        while right < bytes.len() {
+            let b = bytes[right];
+            if set.contains(&b) {
+                ret = ret.max(set.len());
+                while left < right {
+                    set.remove(&bytes[left]);
+                    left += 1;
+
+                    if bytes[left - 1] == b {
+                        break;
+                    }
+                }
             }
-        });
+            right += 1;
+            set.insert(b);
+        }
         ret.max(set.len()) as i32
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -24,9 +39,19 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(Solution::length_of_longest_substring("abcabcbb".to_string()), 3);
+        assert_eq!(
+            Solution::length_of_longest_substring("abcaefg".to_string()),
+            6
+        );
+        assert_eq!(
+            Solution::length_of_longest_substring("abcabcbb".to_string()),
+            3
+        );
         assert_eq!(Solution::length_of_longest_substring("bbbb".to_string()), 1);
-        assert_eq!(Solution::length_of_longest_substring("pwwkew".to_string()), 3);
+        assert_eq!(
+            Solution::length_of_longest_substring("pwwkew".to_string()),
+            3
+        );
         assert_eq!(Solution::length_of_longest_substring("".to_string()), 0);
         assert_eq!(Solution::length_of_longest_substring("a".to_string()), 1);
         assert_eq!(Solution::length_of_longest_substring("aa".to_string()), 1);
