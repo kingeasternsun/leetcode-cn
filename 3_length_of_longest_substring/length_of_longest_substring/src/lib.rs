557b8022b077f3d7177bfd53dacf6c9740a25039
kingeasternsun <kingeasternsun@gmail.com>
2023-08-02 02:54:34 +0000

    3 length_of_longest_substring

--- 3_length_of_longest_substring/length_of_longest_substring/src/lib.rs
+++ 3_length_of_longest_substring/length_of_longest_substring/src/lib.rs
@@ -1,18 +1,17 @@
 struct Solution;
 
 impl Solution {
+    /*
+    这个实现方法中，利用HashSet来记录当前的最长子串的所有字符，然后用来判断字符是否在子串中已经存在
+    但是发现相同字符后，需要把之前的相同字符b以及b之前的字符从set中移除
+     */
     pub fn length_of_longest_substring(s: String) -> i32 {
         let mut set = std::collections::HashSet::new();
         let mut left = 0;
         let mut right = 0;
         let mut ret = 0;
         let bytes = s.as_bytes();
-        // s.as_bytes().into_iter().for_each(|b| {
-        //     if set.contains(b) {
-        //         ret = ret.max(set.len());
-        //     }
-        //     set.insert(b);
-        // });
+
         while right < bytes.len() {
             let b = bytes[right];
             if set.contains(&b) {
@@ -30,6 +29,39 @@ impl Solution {
             set.insert(b);
         }
         ret.max(set.len()) as i32
+    }
+
+    /*
+        在这个实现方法中，通过记录每个字符最近一次在s中的位置，从而快速判断字符是否在子串中存在
+    ┌─┬─┬─┬─┬─┬─┐
+    │ │i│ │x│ │j│
+    └─┴─┴─┴─┴─┴─┘
+    如图，假设双指标分别指向i和j，记录s从i到j-1的子串(不包含j)，s[j]最近一次在s中出现的位置为x，
+    如果 x>=i, 那么说明 s[j] 在子串s[i:j)中存在
+         */
+    pub fn length_of_longest_substring2(s: String) -> i32 {
+        if s.as_bytes().len() == 0 {
+            return 0;
+        }
+        let mut pre_pos = vec![-1; 128];
+        let mut left = -1;
+        let mut right = 0;
+        let mut ret = 0;
+        let bytes = s.as_bytes();
+
+        while right < bytes.len() {
+            let b = bytes[right];
+            let x = pre_pos[b as usize];
+            if x >= left {
+                // 注意 pre_pos,left初始化为-1， right 为0的时候会进入这里，保证正确性
+                ret = ret.max(right as i32 - left);
+                left = x + 1;
+            }
+
+            pre_pos[b as usize] = right as i32;
+            right += 1;
+        }
+        ret.max(right as i32 - left)
     }
 }
 
@@ -55,7 +87,34 @@ mod tests {
         assert_eq!(Solution::length_of_longest_substring("".to_string()), 0);
         assert_eq!(Solution::length_of_longest_substring("a".to_string()), 1);
         assert_eq!(Solution::length_of_longest_substring("aa".to_string()), 1);
+        assert_eq!(Solution::length_of_longest_substring("aab".to_string()), 2);
         assert_eq!(Solution::length_of_longest_substring("ab".to_string()), 2);
         assert_eq!(Solution::length_of_longest_substring("abc".to_string()), 3);
+    }
+
+    #[test]
+    fn it_works2() {
+        assert_eq!(
+            Solution::length_of_longest_substring2("abcaefg".to_string()),
+            6
+        );
+        assert_eq!(
+            Solution::length_of_longest_substring2("abcabcbb".to_string()),
+            3
+        );
+        assert_eq!(
+            Solution::length_of_longest_substring2("bbbb".to_string()),
+            1
+        );
+        assert_eq!(
+            Solution::length_of_longest_substring2("pwwkew".to_string()),
+            3
+        );
+        assert_eq!(Solution::length_of_longest_substring2("".to_string()), 0);
+        assert_eq!(Solution::length_of_longest_substring2("a".to_string()), 1);
+        assert_eq!(Solution::length_of_longest_substring2("aa".to_string()), 1);
+        assert_eq!(Solution::length_of_longest_substring2("ab".to_string()), 2);
+        assert_eq!(Solution::length_of_longest_substring2("aab".to_string()), 2);
+        assert_eq!(Solution::length_of_longest_substring2("abc".to_string()), 3);
     }
 }
