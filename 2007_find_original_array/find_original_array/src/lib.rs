ef997579977d94d7901aabcccb2cb4457c4d349a
kingeasternsun <kingeasternsun@gmail.com>
2023-08-05 12:27:00 +0800

    Rust 200 find_original_array hashmap中只记录 2*n: 76ms 3.67mb

--- 2007_find_original_array/find_original_array/src/lib.rs
+++ 2007_find_original_array/find_original_array/src/lib.rs
@@ -13,6 +13,8 @@ impl Solution {
     // 算法：每次移除最小值和对应的double值
     // 使用 排序 和 HashMap
     // 注意点： 0 的 2倍 也是 0
+
+    // 方法1 用hashMap 来模拟移除 : 76ms 4.73mb
     pub fn find_original_array(mut changed: Vec<i32>) -> Vec<i32> {
         if changed.len() & 1 == 1 {
             return vec![];
@@ -21,14 +23,16 @@ impl Solution {
 
         changed.sort_unstable();
 
-        let mut map: std::collections::HashMap<i32, i32> = std::collections::HashMap::new();
         let mut ret = vec![];
+
+        let mut map: std::collections::HashMap<i32, i32> = std::collections::HashMap::new();
         changed.iter().for_each(|x| {
             *map.entry(*x).or_default() += 1;
         });
 
         for n in changed {
             let v1 = map.get(&n).unwrap().clone();
+            // 已经在前面步骤被移除了
             if v1 == 0 {
                 continue;
             }
@@ -36,6 +40,7 @@ impl Solution {
             // 移除一个n， 注意 n 可能为0，所以要放在get 2*n 之前
             map.insert(n, v1 - 1);
 
+            // 判断 2n 是否存在
             let v2 = map.get(&(2 * n)).map_or(0, |v| v.clone());
             if v2 == 0 {
                 break;
@@ -47,6 +52,44 @@ impl Solution {
 
         // println!("{:?}", ret);
         if ret.len() == orgin_len {
+            return ret;
+        }
+
+        return vec![];
+    }
+
+    // 方法2 hashmap中只记录 2*n: 76ms 3.67mb
+    pub fn find_original_array2(mut changed: Vec<i32>) -> Vec<i32> {
+        if changed.len() & 1 == 1 {
+            return vec![];
+        }
+        let orgin_len = changed.len() / 2;
+
+        changed.sort_unstable();
+
+        let mut ret = vec![];
+
+        let mut map: std::collections::HashMap<i32, i32> = std::collections::HashMap::new();
+
+        for n in changed {
+            match map.get(&n) {
+                Some(&v) => {
+                    // double value
+                    if v == 1 {
+                        map.remove(&n);
+                    } else {
+                        map.insert(n, v - 1);
+                    }
+                }
+                None => {
+                    // origin value
+                    ret.push(n);
+                    *map.entry(2 * n).or_insert(0) += 1;
+                }
+            }
+        }
+
+        if map.len() == 0 {
             return ret;
         }
 
@@ -68,11 +111,38 @@ mod tests {
             Solution::find_original_array(vec![1, 2, 3, 4, 6, 8]),
             vec![1, 3, 4]
         );
+
+        assert_eq!(
+            Solution::find_original_array(vec![1, 2, 3, 6, 3, 6]),
+            vec![1, 3, 3]
+        );
         assert_eq!(Solution::find_original_array(vec![0, 1, 1, 0]), vec![]);
         assert_eq!(Solution::find_original_array(vec![0, 1, 2, 0]), vec![0, 1]);
         assert_eq!(Solution::find_original_array(vec![0, 1, 0, 0]), vec![]);
         assert_eq!(Solution::find_original_array(vec![0, 1]), vec![]);
         assert_eq!(Solution::find_original_array(vec![0, 0]), vec![0]);
         assert_eq!(Solution::find_original_array(vec![0, 0, 0, 0]), vec![0, 0]);
+    }
+
+    #[test]
+    fn it_works2() {
+        assert_eq!(
+            Solution::find_original_array2(vec![1, 3, 4, 2, 6, 8]),
+            vec![1, 3, 4]
+        );
+        assert_eq!(
+            Solution::find_original_array2(vec![1, 2, 3, 4, 6, 8]),
+            vec![1, 3, 4]
+        );
+        assert_eq!(
+            Solution::find_original_array2(vec![1, 2, 3, 6, 3, 6]),
+            vec![1, 3, 3]
+        );
+        assert_eq!(Solution::find_original_array2(vec![0, 1, 1, 0]), vec![]);
+        assert_eq!(Solution::find_original_array2(vec![0, 1, 2, 0]), vec![0, 1]);
+        assert_eq!(Solution::find_original_array2(vec![0, 1, 0, 0]), vec![]);
+        assert_eq!(Solution::find_original_array2(vec![0, 1]), vec![]);
+        assert_eq!(Solution::find_original_array2(vec![0, 0]), vec![0]);
+        assert_eq!(Solution::find_original_array2(vec![0, 0, 0, 0]), vec![0, 0]);
     }
 }
