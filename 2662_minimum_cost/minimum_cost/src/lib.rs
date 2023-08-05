37920aaf004862b33c9f47e52407924f523d0390
kingeasternsun <kingeasternsun@gmail.com>
2023-08-05 12:29:04 +0800

    Rust 2008_max_taxi_earnings 双100 376ms 6.5mb

--- 2662_minimum_cost/minimum_cost/src/lib.rs
+++ 2662_minimum_cost/minimum_cost/src/lib.rs
@@ -99,17 +99,31 @@ impl Solution {
     // }
 }
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(
+            Solution::minimum_cost(
+                vec![1, 1],
+                vec![4, 5],
+                vec![vec![1, 2, 3, 3, 2], vec![3, 4, 4, 5, 1]]
+            ),
+            5
+        );
+        assert_eq!(
+            Solution::minimum_cost(
+                vec![3, 2],
+                vec![5, 7],
+                vec![
+                    vec![3, 2, 3, 4, 4],
+                    vec![3, 3, 5, 5, 5],
+                    vec![3, 4, 5, 6, 6]
+                ]
+            ),
+            7
+        );
     }
 }
