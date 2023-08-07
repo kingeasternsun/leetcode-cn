64885ebfbe9bbaf12cd0125b3cafa7799362b242
kingeasternsun <kingeasternsun@gmail.com>
2023-08-07 01:20:06 +0000

    Rust 244_reverse_string

---
+++ 244_reverse_string/reverse_string/src/lib.rs
@@ -0,0 +1,18 @@
+
+struct Solution;
+
+impl Solution {
+    pub fn reverse_string(s:&mut Vec<char>){
+        s.reverse()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+    }
+}
