c7a953fc1a215e79b54e168e034bca7a05e5729f
kingeasternsun <kingeasternsun@gmail.com>
2023-08-08 22:03:15 +0800

    Rust 1752_check

--- 1752_check/check/src/lib.rs
+++ 1752_check/check/src/lib.rs
@@ -2,7 +2,28 @@ struct Solution;
 
 impl Solution {
     // 找到拐点，也就是第一个递减的元素，从这个位置再判断是否单调非递减
+    // beauty of iterator
     pub fn check(nums: Vec<i32>) -> bool {
+        use std::ops::ControlFlow;
+        let r = nums.windows(2).enumerate().try_for_each(|(n, x)| {
+            if x[0] <= x[1] {
+                ControlFlow::Continue(())
+            } else {
+                ControlFlow::Break(n) //拐点 n+1
+            }
+        });
+
+        match r {
+            ControlFlow::Continue(_) => return true, // 没有拐点
+            ControlFlow::Break(n) => {
+                return nums[n + 1..].windows(2).all(|x| x[0] <= x[1])
+                    && nums[nums.len() - 1] <= nums[0]
+            }
+        }
+    }
+
+    // 找到拐点，也就是第一个递减的元素，从这个位置再判断是否单调非递减
+    pub fn check1(nums: Vec<i32>) -> bool {
         // 找到拐点
         let mut min = 0;
         for i in 1..nums.len() {
