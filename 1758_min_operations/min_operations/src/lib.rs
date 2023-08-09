c0d79f1bf223f1f0959a8526c906fae626d379fa
kingeasternsun <kingeasternsun@gmail.com>
2023-08-09 02:11:44 +0000

    Rust 1758_min_operations

--- 1758_min_operations/min_operations/src/lib.rs
+++ 1758_min_operations/min_operations/src/lib.rs
@@ -20,6 +20,25 @@ impl Solution {
 
         ret1.min(ret0)
     }
+
+    // 代码简化
+    pub fn min_operations1(s: String) -> i32 {
+        let exp = "01".as_bytes();
+        let bytes = s.as_bytes();
+        let mut ret0 = 0;
+        let mut ret1 = 0;
+        for (id, &b) in bytes.iter().enumerate() {
+            if b != exp[id & 1] {
+                ret0 += 1;
+            }
+
+            if b != exp[(id + 1) & 1] {
+                ret1 += 1;
+            }
+        }
+
+        ret1.min(ret0)
+    }
 }
 
 #[cfg(test)]
@@ -37,5 +56,18 @@ mod tests {
         assert_eq!(Solution::min_operations(String::from("1111")), 2);
         assert_eq!(Solution::min_operations(String::from("0")), 0);
         assert_eq!(Solution::min_operations(String::from("1")), 0);
+    }
+
+    #[test]
+    fn it_works1() {
+        assert_eq!(Solution::min_operations1(String::from("0100")), 1);
+        assert_eq!(Solution::min_operations1(String::from("1000")), 1);
+        assert_eq!(Solution::min_operations1(String::from("10")), 0);
+        assert_eq!(Solution::min_operations1(String::from("01")), 0);
+        assert_eq!(Solution::min_operations1(String::from("101")), 0);
+        assert_eq!(Solution::min_operations1(String::from("010")), 0);
+        assert_eq!(Solution::min_operations1(String::from("1111")), 2);
+        assert_eq!(Solution::min_operations1(String::from("0")), 0);
+        assert_eq!(Solution::min_operations1(String::from("1")), 0);
     }
 }
