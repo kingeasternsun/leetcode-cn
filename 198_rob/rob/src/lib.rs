296520cc4712a94c384c2067cefe96bcc2566e2d
kingeasternsun <kingeasternsun@gmail.com>
2023-08-09 23:03:14 +0800

    Rust 198_rob

--- 198_rob/rob/src/lib.rs
+++ 198_rob/rob/src/lib.rs
@@ -6,6 +6,8 @@ impl Solution {
         Self::dfs(&nums[0..], 0, &mut max_rob);
         max_rob
     }
+    // 对于每一户，对应偷或不偷两种状态，最终是求在不同组合下的结果，典型的DFS题型
+    // LC: timeout
     fn dfs(nums: &[i32], cur_sum: i32, max_rob: &mut i32) {
         if nums.len() == 0 {
             *max_rob = cur_sum.max(*max_rob);
@@ -33,6 +35,7 @@ impl Solution {
     //  从第 i+2 户开始的最大值， 表示抢第 i+1 户
     // 的最大值相关
     // dp[i] = max( dp[i+1] ,nums[i] + dp[i-2] )
+    // LC: timeout
     fn dp(nums: &[i32]) -> i32 {
         if nums.len() <= 1 {
             return nums.iter().sum();
@@ -43,6 +46,23 @@ impl Solution {
         // not rob cur
         let ret2 = Self::dp(&nums[1..]);
         ret1.max(ret2)
+    }
+
+    pub fn rob2(nums: Vec<i32>) -> i32 {
+        if nums.len()==1{
+            return nums[0]
+        }
+
+        let mut a1 =  nums[nums.len()-1];
+        let mut a2 = 0;
+        let mut cur = 0;
+        for id in (0..nums.len()-1).rev(){
+            cur = a1.max(nums[id] + a2);
+            a2 = a1;
+            a1 = cur;
+        }
+
+        a1.max(a2)
     }
 }
 
@@ -64,5 +84,13 @@ mod tests {
         assert_eq!(Solution::rob1(vec![2, 7, 9, 3, 1]), 12);
         assert_eq!(Solution::rob1(vec![1]), 1);
         assert_eq!(Solution::rob1(vec![1, 2]), 2);
+    }
+
+    #[test]
+    fn it_works2() {
+        assert_eq!(Solution::rob2(vec![1, 2, 3, 1]), 4);
+        assert_eq!(Solution::rob2(vec![2, 7, 9, 3, 1]), 12);
+        assert_eq!(Solution::rob2(vec![1]), 1);
+        assert_eq!(Solution::rob2(vec![1, 2]), 2);
     }
 }
--- 337_rob/rob/src/lib.rs
+++ 337_rob/rob/src/lib.rs
@@ -11,12 +11,13 @@ pub struct TreeNode {
 
 impl Solution {
     pub fn rob(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        Self::dp(root.clone(), true)
+        Self::dp(root.clone(), false)
     }
 
     // 由于二叉树无法像数组那样直接跳到孙子节点，所以只能一级一级跳，通过一个can_rob 标识当前节点是否要跳过
     // 从当前节点开始，rob 当前节点可以得到的最大值 和 不rob 当前节点可以得到的最大值
     // 如果父节点被rob了， 当前节点就不能rob
+    // 122 / 124 个通过的测试用例 timeout
     fn dp(root: Option<Rc<RefCell<TreeNode>>>, skip: bool) -> i32 {
         if root.is_none() {
             return 0;
