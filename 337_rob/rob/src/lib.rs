a90776e6cb9566aae3e65267375e46b1b2608085
kingeasternsun <kingeasternsun@gmail.com>
2023-08-09 09:59:55 +0000

    Rust 337_rob

--- 337_rob/rob/src/lib.rs
+++ 337_rob/rob/src/lib.rs
@@ -16,26 +16,27 @@ impl Solution {
         Self::dp(root.clone(),true)
     }
 
+    // 由于二叉树无法像数组那样直接跳到孙子节点，所以只能一级一级跳，通过一个can_rob 标识当前节点是否要跳过
     // 从当前节点开始，rob 当前节点可以得到的最大值 和 不rob 当前节点可以得到的最大值
     // 如果父节点被rob了， 当前节点就不能rob
-    fn dp(root: Option<Rc<RefCell<TreeNode>>>, can_rob: bool) -> i32 {
+    fn dp(root: Option<Rc<RefCell<TreeNode>>>, skip: bool) -> i32 {
         if root.is_none() {
             return 0;
         }
 
         let cur = root.unwrap();
 
-        if can_rob {
+        if !skip {
             // rob cur
-            let ret1= cur.as_ref().borrow().val + Self::dp(cur.as_ref().borrow().left.clone(), false) + Self::dp(cur.as_ref().borrow().right.clone(),false);
+            let ret1= cur.as_ref().borrow().val + Self::dp(cur.as_ref().borrow().left.clone(), true) + Self::dp(cur.as_ref().borrow().right.clone(),true);
 
             // not rob cur
-            let ret0 =  Self::dp(cur.as_ref().borrow().left.clone(), true) + Self::dp(cur.as_ref().borrow().right.clone(),true);
+            let ret0 =  Self::dp(cur.as_ref().borrow().left.clone(), false) + Self::dp(cur.as_ref().borrow().right.clone(),false);
             return ret1.max(ret0);
         }
 
         // can not rob cur
-        return Self::dp(cur.as_ref().borrow().left.clone(), true) + Self::dp(cur.as_ref().borrow().right.clone(),true);
+        return Self::dp(cur.as_ref().borrow().left.clone(), false) + Self::dp(cur.as_ref().borrow().right.clone(),false);
     }
 }
 
