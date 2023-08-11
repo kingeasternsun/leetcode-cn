1343e90848cc3c85297e15d8cdabcf15011413ee
kingeasternsun <kingeasternsun@gmail.com>
2023-08-11 07:15:17 +0000

    Rust 1573_num_ways

---
+++ 1573_num_ways/num_ways/src/lib.rs
@@ -0,0 +1,64 @@
+struct Solution;
+
+impl Solution {
+    // 统计'1'的个数，然后看是否可以被3整除
+    // 如果可以被3整除，假设得到m
+    // 那么根据第 m,m+1 个'1'所在的索引，以及 2m,2m+1 个'1'所在的索引就可以算出来
+    pub fn num_ways(s: String) -> i32 {
+        let bytes = s.as_bytes();
+        let num1 = bytes.iter().filter(|&x| *x == b'1').count();
+        if num1 == 0 {
+            return ((bytes.len() - 1) * (bytes.len() - 2) / 2 % 1000000007)
+                .try_into()
+                .unwrap();
+        }
+
+        if num1 % 3 > 0 {
+            return 0;
+        }
+
+        // println!("{}", num1);
+
+        let m = num1 / 3;
+        let mut first = (0, 0);
+        let mut second = (0, 0);
+        let mut cnt = 0;
+        for i in 0..bytes.len() {
+            if bytes[i] == b'1' {
+                cnt += 1;
+                if cnt == m {
+                    first.0 = i
+                }
+
+                if cnt == m + 1 {
+                    first.1 = i
+                }
+                if cnt == 2 * m {
+                    second.0 = i
+                }
+                if cnt == 2 * m + 1 {
+                    second.1 = i
+                }
+            }
+        }
+
+        // println!("{:?}{:?}", first, second);
+
+        ((first.1 - first.0) * (second.1 - second.0) % 1000000007)
+            .try_into()
+            .unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(Solution::num_ways(String::from("10101")), 4);
+        assert_eq!(Solution::num_ways(String::from("1001")), 0);
+        assert_eq!(Solution::num_ways(String::from("0000")), 3);
+        assert_eq!(Solution::num_ways(String::from("100100010100110")), 12);
+    }
+}
