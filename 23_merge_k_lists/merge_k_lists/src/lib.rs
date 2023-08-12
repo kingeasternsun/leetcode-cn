e4d42d5c695697dceca7eb04f01132b0997456d8
kingeasternsun <kingeasternsun@gmail.com>
2023-08-12 22:47:11 +0800

    Rust 23_merge_k_lists

--- 23_merge_k_lists/merge_k_lists/src/lib.rs
+++ 23_merge_k_lists/merge_k_lists/src/lib.rs
@@ -1,5 +1,3 @@
-
-
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -16,18 +14,17 @@ impl ListNode {
 struct Solution;
 
 use std::cmp::Ordering;
-impl Ord for ListNode{
-    fn cmp(&self, other: &Self)->Ordering{
+impl Ord for ListNode {
+    fn cmp(&self, other: &Self) -> Ordering {
         other.val.cmp(&self.val)
     }
 }
 
-impl PartialOrd for ListNode{
-    fn partial_cmp(&self, other: &Self)->Option<Ordering>{
+impl PartialOrd for ListNode {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
-
 
 impl Solution {
     // 8ms 3.15mb
@@ -41,24 +38,23 @@ impl Solution {
 
         let mut min_heap = std::collections::BinaryHeap::new();
 
-        for node in lists{
-            if node.is_some(){
-                min_heap.push(node.unwrap());
+        for node in lists {
+            if let Some(n) = node {
+                min_heap.push(n)
             }
         }
 
         let mut head = None;
-        let mut pre_next = & mut head;
+        let mut pre_next = &mut head;
 
-        while let Some(mut cur) = min_heap.pop(){
+        while let Some(mut cur) = min_heap.pop() {
             // 断链 cur - X -> cur.next
-            let cur_next = cur.next.take();
-            if cur_next.is_some(){
-                min_heap.push(cur_next.unwrap());
+            if let Some(cur_next) = cur.next.take() {
+                min_heap.push(cur_next)
             }
 
             // cur 填入到 pre_next, 然后 pre_next 更新为 cur.next
-            pre_next = & mut pre_next.insert(cur).next;
+            pre_next = &mut pre_next.insert(cur).next;
         }
 
         head
