08df41791ca393bff71da3d1195d57ab0d3bf756
kingeasternsun <kingeasternsun@gmail.com>
2023-08-14 02:24:08 +0000

    Rust 617_merge_tree

--- 617_merge_tree/merge_tree/src/lib.rs
+++ 617_merge_tree/merge_tree/src/lib.rs
@@ -16,6 +16,28 @@ impl Solution {
     ) -> Option<Rc<RefCell<TreeNode>>> {
         match (root1.clone(), root2.clone()) {
             (Some(r1), Some(r2)) => {
+                let r1_child_left = r1.borrow_mut().left.take();
+                let r1_child_right = r1.borrow_mut().right.take();
+
+                let r2_child_left = r2.borrow_mut().left.take();
+                let r2_child_right = r2.borrow_mut().right.take();
+
+                Some(Rc::new(RefCell::new(TreeNode {
+                    val: r1.borrow().val + r2.borrow().val,
+                    left: Self::merge_trees(r1_child_left, r2_child_left),
+                    right: Self::merge_trees(r1_child_right, r2_child_right),
+                })))
+            }
+            _ => root1.or(root2),
+        }
+    }
+
+    pub fn merge_trees1(
+        root1: Option<Rc<RefCell<TreeNode>>>,
+        root2: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        match (root1.clone(), root2.clone()) {
+            (Some(r1), Some(r2)) => {
                 let r1_child = (r1.borrow_mut().left.take(), r1.borrow_mut().right.take());
                 let r2_child = (r2.borrow_mut().left.take(), r2.borrow_mut().right.take());
 
@@ -33,7 +55,10 @@ impl Solution {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use leetcode_prelude::btree;
 
     #[test]
-    fn it_works() {}
+    fn it_works() {
+        let btree = btree![1, 2, 2, null, null, 3, 3];
+    }
 }
