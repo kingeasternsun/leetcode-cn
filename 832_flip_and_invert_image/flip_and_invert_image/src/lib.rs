fc67923489d0de3844846232995638231210f610
kingeasternsun <kingeasternsun@gmail.com>
2023-08-15 10:02:33 +0000

    832_flip_and_invert_image

---
+++ 832_flip_and_invert_image/flip_and_invert_image/src/lib.rs
@@ -0,0 +1,57 @@
+struct Solution;
+impl Solution {
+    pub fn flip_and_invert_image(image: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut image = image;
+        image.iter_mut().for_each(|row| {
+            let (mut left, mut right) = (0, row.len() - 1);
+            while left < right {
+                row.swap(left, right);
+                left += 1;
+                right -= 1;
+            }
+            for i in 0..row.len() {
+                row[i] = 1 - row[i];
+            }
+        });
+
+        image
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            Solution::flip_and_invert_image(vec![vec![1, 1, 0], vec![1, 0, 1], vec![0, 0, 0]]),
+            vec![vec![1, 0, 0], vec![0, 1, 0], vec![1, 1, 1]]
+        );
+
+        assert_eq!(
+            Solution::flip_and_invert_image(vec![
+                vec![1, 1, 0, 0],
+                vec![1, 0, 0, 1],
+                vec![0, 1, 1, 1],
+                vec![1, 0, 1, 0]
+            ]),
+            vec![
+                vec![1, 1, 0, 0],
+                vec![0, 1, 1, 0],
+                vec![0, 0, 0, 1],
+                vec![1, 0, 1, 0]
+            ]
+        );
+
+        assert_eq!(
+            Solution::flip_and_invert_image(vec![vec![0, 1], vec![1, 0]]),
+            vec![vec![0, 1], vec![1, 0]]
+        );
+
+        assert_eq!(
+            Solution::flip_and_invert_image(vec![vec![1]]),
+            vec![vec![0]]
+        );
+    }
+}
