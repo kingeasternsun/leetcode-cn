89d3c9aeaab8a43a021c6265cabd7ba25e640a77
kingeasternsun <kingeasternsun@gmail.com>
2023-08-15 11:31:10 +0000

    Rust 845_longest_mountain

---
+++ 845_longest_mountain/longest_mountain/src/lib.rs
@@ -0,0 +1,81 @@
+struct Solution;
+
+enum Direction {
+    Up,
+    Down,
+    Equal,
+}
+
+impl Solution {
+    pub fn longest_mountain(arr: Vec<i32>) -> i32 {
+        let mut ret = 0;
+        let mut pre_dir = Direction::Equal;
+        let mut pre_len = 0;
+        arr.windows(2).for_each(|x| {
+            match pre_dir {
+                Direction::Equal => {
+                    if x[0] < x[1] {
+                        // 上山开始
+                        pre_len = 2;
+                        pre_dir = Direction::Up;
+                    }
+                }
+                Direction::Up => {
+                    if x[0] < x[1] {
+                        // 继续上山
+                        pre_len += 1;
+                    } else if x[0] == x[1] {
+                        pre_len = 0;
+                        pre_dir = Direction::Equal;
+                    } else {
+                        // 到达山顶，开始下山
+                        pre_len += 1;
+                        pre_dir = Direction::Down;
+                    }
+                }
+                Direction::Down => {
+                    if x[0] < x[1] {
+                        // 到达山谷，开始爬新的山
+                        ret = ret.max(pre_len);
+                        pre_len = 0;
+                        pre_dir = Direction::Up;
+                    } else if x[0] == x[1] {
+                        ret = ret.max(pre_len);
+                        pre_len = 0;
+                        pre_dir = Direction::Equal;
+                    } else {
+                        // 继续下山
+                        pre_len += 1;
+                    }
+                }
+            }
+        });
+
+        // 特殊case的处理
+        match pre_dir {
+            Direction::Down => ret = ret.max(pre_len),
+            _ => {}
+        }
+
+        ret
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(Solution::longest_mountain(vec![2, 1, 4, 7, 3, 2, 5]), 5);
+
+        assert_eq!(Solution::longest_mountain(vec![2, 2, 1, 2]), 0);
+        assert_eq!(Solution::longest_mountain(vec![2, 2, 4, 2]), 3);
+        assert_eq!(Solution::longest_mountain(vec![2, 2, 4, 4, 2]), 0);
+        assert_eq!(Solution::longest_mountain(vec![2, 2, 2]), 0);
+        assert_eq!(Solution::longest_mountain(vec![2, 2, 4]), 0);
+        assert_eq!(Solution::longest_mountain(vec![2, 2, 1]), 0);
+        assert_eq!(Solution::longest_mountain(vec![2, 4]), 0);
+        assert_eq!(Solution::longest_mountain(vec![2, 1]), 0);
+    }
+}
