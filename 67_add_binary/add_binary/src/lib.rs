bf730a586a1721325d15632c73b696bcc34420c6
kingeasternsun <kingeasternsun@gmail.com>
2023-08-15 23:33:29 +0800

    Rust 67_add_binary

---
+++ 67_add_binary/add_binary/src/lib.rs
@@ -0,0 +1,20 @@
+struct Solution;
+impl Solution {
+    pub fn add_binary(a: String, b: String) -> String {
+        
+
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(Solution::add_binary("11".to_string(),"1".to_string()), "100".to_string());
+        assert_eq!(Solution::add_binary("1010".to_string(),"1011".to_string()), "10101".to_string());
+    }
+}
