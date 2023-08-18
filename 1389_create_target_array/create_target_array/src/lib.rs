e486dd5b97020d0ea9eda7ac6412eb85ff10d950
kingeasternsun <kingeasternsun@gmail.com>
2023-08-18 02:03:15 +0000

    Rust 1389_create_target_array

--- 1389_create_target_array/create_target_array/src/lib.rs
+++ 1389_create_target_array/create_target_array/src/lib.rs
@@ -1,17 +1,36 @@
 struct Solution;
 impl Solution {
+    // 0ms 2.1mb
     pub fn create_target_array(nums: Vec<i32>, index: Vec<i32>) -> Vec<i32> {
         let mut target = vec![0; nums.len()];
         let origin_len = nums.len();
         index.into_iter().zip(nums.into_iter()).for_each(|(id, n)| {
-            if target[id as usize] == -1 {
-                target[id as usize] = n;
-            } else {
-                target.insert(id as usize, n);
-            }
+            target.insert(id as usize, n);
         });
 
         target.truncate(origin_len);
+        target
+    }
+
+    pub fn create_target_array1(nums: Vec<i32>, index: Vec<i32>) -> Vec<i32> {
+        let mut target = vec![];
+        index.into_iter().zip(nums.into_iter()).for_each(|(id, n)| {
+            target.insert(id as usize, n);
+        });
+        target
+    }
+
+    pub fn create_target_array2(nums: Vec<i32>, index: Vec<i32>) -> Vec<i32> {
+        let mut target = vec![-1; nums.len()];
+        let len = nums.len();
+        index.into_iter().zip(nums.into_iter()).for_each(|(id, n)| {
+            if target[id as usize] == -1 {
+                target[id as usize] = n;
+            } else {
+                target.copy_within(id as usize..len - 1, id as usize + 1);
+                target[id as usize] = n;
+            }
+        });
         target
     }
 }
@@ -32,5 +51,33 @@ mod tests {
             vec![0, 1, 2, 3, 4]
         );
         assert_eq!(Solution::create_target_array(vec![1], vec![0]), vec![1]);
+    }
+
+    #[test]
+    fn it_works1() {
+        assert_eq!(
+            Solution::create_target_array1(vec![0, 1, 2, 3, 4], vec![0, 1, 2, 2, 1]),
+            vec![0, 4, 1, 3, 2]
+        );
+
+        assert_eq!(
+            Solution::create_target_array1(vec![1, 2, 3, 4, 0], vec![0, 1, 2, 3, 0]),
+            vec![0, 1, 2, 3, 4]
+        );
+        assert_eq!(Solution::create_target_array1(vec![1], vec![0]), vec![1]);
+    }
+
+    #[test]
+    fn it_works2() {
+        assert_eq!(
+            Solution::create_target_array2(vec![0, 1, 2, 3, 4], vec![0, 1, 2, 2, 1]),
+            vec![0, 4, 1, 3, 2]
+        );
+
+        assert_eq!(
+            Solution::create_target_array2(vec![1, 2, 3, 4, 0], vec![0, 1, 2, 3, 0]),
+            vec![0, 1, 2, 3, 4]
+        );
+        assert_eq!(Solution::create_target_array2(vec![1], vec![0]), vec![1]);
     }
 }
