e8b6a61c5abae90a1536f2be5dd1a2f79ffffa7e
kingeasternsun <kingeasternsun@gmail.com>
2023-08-18 01:16:58 +0000

    Rust 1446_max_power

--- 1446_max_power/max_power/src/lib.rs
+++ 1446_max_power/max_power/src/lib.rs
@@ -24,6 +24,29 @@ impl Solution {
 
         ret
     }
+
+    pub fn max_power1(s: String) -> i32 {
+        let mut ret = 1;
+        let mut pre = None;
+        s.into_bytes().windows(2).for_each(|win| {
+            if win[0] == win[1] {
+                match pre {
+                    None => {
+                        pre = Some(2);
+                        ret = ret.max(2);
+                    }
+                    Some(pre_len) => {
+                        pre.replace(pre_len + 1);
+                        ret = ret.max(pre_len + 1);
+                    }
+                }
+            } else if let Some(pre_len) = pre {
+                pre = None;
+            }
+        });
+
+        ret
+    }
 }
 
 #[cfg(test)]
@@ -38,5 +61,15 @@ mod tests {
         assert_eq!(Solution::max_power("bbaaa".to_string()), 3);
         assert_eq!(Solution::max_power("aa".to_string()), 2);
         assert_eq!(Solution::max_power("a".to_string()), 1);
+    }
+
+    #[test]
+    fn it_works1() {
+        assert_eq!(Solution::max_power1("leetcode".to_string()), 2);
+        assert_eq!(Solution::max_power1("abbcccdddeeeeedcba".to_string()), 5);
+        assert_eq!(Solution::max_power1("abaaa".to_string()), 3);
+        assert_eq!(Solution::max_power1("bbaaa".to_string()), 3);
+        assert_eq!(Solution::max_power1("aa".to_string()), 2);
+        assert_eq!(Solution::max_power1("a".to_string()), 1);
     }
 }
