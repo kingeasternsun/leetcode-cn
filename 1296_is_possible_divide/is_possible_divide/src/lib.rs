9f07d9c4738de9f7875df3090c801789f8e71e69
kingeasternsun <kingeasternsun@gmail.com>
2023-08-18 06:39:47 +0000

    Rust 1296_is_possible_divide

--- 1296_is_possible_divide/is_possible_divide/src/lib.rs
+++ 1296_is_possible_divide/is_possible_divide/src/lib.rs
@@ -7,45 +7,34 @@ impl Solution {
             return false;
         }
 
-        let iter_cnt = nums.len() / k;
+        let mut nums = nums;
+        nums.sort_unstable();
 
-        // 求最大，最小值
-        let min = nums.iter().min().map_or(0, |x| x.clone()) as usize;
-        let max = nums.iter().max().map_or(0, |x| x.clone()) as usize;
-        // 逆向推理，假设可以拆分，那么极端情况下 nums中是一个完全顺子，最大值和最小值的差为
-        if max - min >= nums.len() {
-            return false;
-        }
-
-        let mut num_map = vec![0; max - min + 1];
-        nums.into_iter().for_each(|x| {
-            num_map[x as usize - min] += 1;
+        let mut map = std::collections::HashMap::new();
+        nums.iter().for_each(|&x| {
+            *map.entry(x).or_insert(0) += 1;
         });
 
-        println!("{:?}", num_map);
-        let mut begin_id = 0;
-        for iter in 0..iter_cnt {
-            for i in 0..k {
-                if num_map[begin_id + i] == 0 {
-                    return false;
-                }
-                num_map[begin_id + i] -= 1;
+        for x in nums.into_iter() {
+            let v = map.get(&x).map_or(0, |v| v.clone());
+            if v == 0 {
+                continue;
             }
 
-            let mut next_id = begin_id;
-            while next_id < begin_id + k {
-                if num_map[next_id] > 0 {
-                    break;
+            for k in x..x + k as i32 {
+                match map.get(&k) {
+                    None => return false,
+                    Some(&v) => {
+                        if v == 1 {
+                            map.remove(&k);
+                        } else {
+                            map.insert(k, v - 1);
+                        }
+                    }
                 }
-                next_id += 1;
             }
-
-            begin_id = next_id;
-
-            println!("{:?} {}", num_map, begin_id);
         }
         true
-        // begin_id == num_map.len()
     }
 }
 
@@ -55,18 +44,18 @@ mod tests {
 
     #[test]
     fn it_works() {
-        // assert_eq!(
-        //     Solution::is_possible_divede(vec![1, 2, 3, 3, 4, 4, 5, 6], 4),
-        //     true
-        // );
+        assert_eq!(
+            Solution::is_possible_divede(vec![1, 2, 3, 3, 4, 4, 5, 6], 4),
+            true
+        );
         assert_eq!(
             Solution::is_possible_divede(vec![3, 2, 1, 2, 3, 4, 3, 4, 5, 9, 10, 11], 3),
             true
         );
-        // assert_eq!(
-        //     Solution::is_possible_divede(vec![3, 3, 2, 2, 1, 1], 3),
-        //     true
-        // );
-        // assert_eq!(Solution::is_possible_divede(vec![1, 2, 3, 4], 3), false);
+        assert_eq!(
+            Solution::is_possible_divede(vec![3, 3, 2, 2, 1, 1], 3),
+            true
+        );
+        assert_eq!(Solution::is_possible_divede(vec![1, 2, 3, 4], 3), false);
     }
 }
