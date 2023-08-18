7d4a072749b304bef291c95a7cd7827d84d21888
kingeasternsun <kingeasternsun@gmail.com>
2023-08-18 06:52:59 +0000

    Rust 1296_is_possible_divide

--- 1296_is_possible_divide/is_possible_divide/src/lib.rs
+++ 1296_is_possible_divide/is_possible_divide/src/lib.rs
@@ -1,6 +1,7 @@
 struct Solution;
 impl Solution {
-    pub fn is_possible_divede(nums: Vec<i32>, k: i32) -> bool {
+    // 36ms 4.8mb
+    pub fn is_possible_divide(nums: Vec<i32>, k: i32) -> bool {
         let k = k as usize;
         // 不能整除
         if nums.len() % k > 0 {
@@ -36,6 +37,38 @@ impl Solution {
         }
         true
     }
+
+    pub fn is_possible_divide1(nums: Vec<i32>, k: i32) -> bool {
+        let k = k as usize;
+        // 不能整除
+        if nums.len() % k > 0 {
+            return false;
+        }
+
+        let mut nums = nums;
+        nums.sort_unstable();
+
+        let mut map = std::collections::HashMap::new();
+        nums.iter().for_each(|&x| {
+            *map.entry(x).or_insert(0) += 1;
+        });
+
+        for x in nums.into_iter() {
+            let v = map.get(&x).map_or(0, |v| v.clone());
+            if v == 0 {
+                continue;
+            }
+
+            for k in x..x + k as i32 {
+                if let Some(v) = map.get_mut(&k) {
+                    *v -= 1;
+                } else {
+                    return false;
+                }
+            }
+        }
+        true
+    }
 }
 
 #[cfg(test)]
@@ -45,17 +78,34 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(
-            Solution::is_possible_divede(vec![1, 2, 3, 3, 4, 4, 5, 6], 4),
+            Solution::is_possible_divide(vec![1, 2, 3, 3, 4, 4, 5, 6], 4),
             true
         );
         assert_eq!(
-            Solution::is_possible_divede(vec![3, 2, 1, 2, 3, 4, 3, 4, 5, 9, 10, 11], 3),
+            Solution::is_possible_divide(vec![3, 2, 1, 2, 3, 4, 3, 4, 5, 9, 10, 11], 3),
             true
         );
         assert_eq!(
-            Solution::is_possible_divede(vec![3, 3, 2, 2, 1, 1], 3),
+            Solution::is_possible_divide(vec![3, 3, 2, 2, 1, 1], 3),
             true
         );
-        assert_eq!(Solution::is_possible_divede(vec![1, 2, 3, 4], 3), false);
+        assert_eq!(Solution::is_possible_divide(vec![1, 2, 3, 4], 3), false);
+    }
+
+    #[test]
+    fn it_works1() {
+        assert_eq!(
+            Solution::is_possible_divide1(vec![1, 2, 3, 3, 4, 4, 5, 6], 4),
+            true
+        );
+        assert_eq!(
+            Solution::is_possible_divide1(vec![3, 2, 1, 2, 3, 4, 3, 4, 5, 9, 10, 11], 3),
+            true
+        );
+        assert_eq!(
+            Solution::is_possible_divide1(vec![3, 3, 2, 2, 1, 1], 3),
+            true
+        );
+        assert_eq!(Solution::is_possible_divide1(vec![1, 2, 3, 4], 3), false);
     }
 }
