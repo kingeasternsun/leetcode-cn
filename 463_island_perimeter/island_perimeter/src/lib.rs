7663dcb19ff4c6153a699e81725f7dd87dcd209d
kingeasternsun <kingeasternsun@gmail.com>
2023-08-19 14:57:04 +0800

    Rust 463_island_perimeter

--- 463_island_perimeter/island_perimeter/src/lib.rs
+++ 463_island_perimeter/island_perimeter/src/lib.rs
@@ -1,6 +1,7 @@
 struct Solution;
 impl Solution {
     // BFS 岛屿边界
+    // 28ms 2.3mb
     pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
         let root = Self::get_root(&grid);
         if root.is_none() {
@@ -45,6 +46,48 @@ impl Solution {
         }
         None
     }
+
+    // 24ms 2mb
+    pub fn island_perimeter1(grid: Vec<Vec<i32>>) -> i32 {
+        let rows = grid.len();
+        if grid.get(0).is_none() {
+            return 0;
+        }
+
+        let cols = grid.get(0).unwrap().len();
+
+        let dirs: Vec<i32> = vec![0, 1, 0, -1, 0];
+
+        let mut sum = 0;
+
+        for i in 0..rows {
+            for j in 0..cols {
+                if grid[i][j] == 0 {
+                    continue;
+                }
+
+                //对每一个方向，如果相邻的是水或边界，就加1
+                dirs.windows(2).for_each(|dir| {
+                    let new_x = (i as i32 + dir[0]) as usize;
+                    let new_y = (j as i32 + dir[1]) as usize;
+
+                    if let Some(row) = grid.get(new_x) {
+                        if let Some(next) = row.get(new_y) {
+                            if *next == 0 {
+                                sum += 1;
+                            }
+                        } else {
+                            sum += 1;
+                        }
+                    } else {
+                        sum += 1;
+                    }
+                });
+            }
+        }
+
+        sum
+    }
 }
 
 #[cfg(test)]
@@ -63,8 +106,25 @@ mod tests {
             16
         );
 
-        // assert_eq!(Solution::island_perimeter(vec![vec![1]]), 4);
+        assert_eq!(Solution::island_perimeter(vec![vec![1]]), 4);
 
-        // assert_eq!(Solution::island_perimeter(vec![vec![1, 0]]), 4);
+        assert_eq!(Solution::island_perimeter(vec![vec![1, 0]]), 4);
+    }
+
+    #[test]
+    fn it_works1() {
+        assert_eq!(
+            Solution::island_perimeter1(vec![
+                vec![0, 1, 0, 0],
+                vec![1, 1, 1, 0],
+                vec![0, 1, 0, 0],
+                vec![1, 1, 0, 0]
+            ]),
+            16
+        );
+
+        assert_eq!(Solution::island_perimeter1(vec![vec![1]]), 4);
+
+        assert_eq!(Solution::island_perimeter1(vec![vec![1, 0]]), 4);
     }
 }
